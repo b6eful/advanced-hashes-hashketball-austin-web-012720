896595a398b4b3ddd341d1136ff0348f8d96e38a
Phoenix R <phoenixrabang@gmail.com>
2020-01-20 00:57:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,15 @@
-# Write your code here!
-
-
+require pry
+def hash
+  {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black," "White"],
+      :players => [
+        {
+          :player_name
+        ]
+    }
+  }
 
 
 
