9872a2ad4144f86e8634eae7526a5fc44178f02c
Phoenix R <phoenixrabang@gmail.com>
2020-01-20 00:58:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,17 @@ def hash
       :colors => ["Black," "White"],
       :players => [
         {
-          :player_name
+         :player_name => "Alan Anderson",
+         :number => 0,
+         :shoe => 16,
+         :points => 22,
+         :rebounds => 12,
+         :assists => 12,
+         :steals => 3,
+         :blocks => 1,
+         :slam_dunks => 1
+        },
+        {
         ]
     }
   }
